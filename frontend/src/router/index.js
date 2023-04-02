5a4f636cedcb72e843535d5758b3c630db55992b
missitnoonan <ambielecki@gmail.com>
2023-04-02 19:04:46 -0400

    commentsold :: don't defer components

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,18 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from "../views/pages/HomePage.vue";
 import { useUserStore } from "../stores/user";
 import AuthProvider from "../providers/AuthProvider";
+import ProductList from "../views/pages/Products/ProductList.vue";
+import LoginView from "../views/pages/LoginView.vue";
+import ProductView from "../views/pages/Products/ProductView.vue";
+import InventoryList from "../views/pages/Inventory/InventoryList.vue";
+import InventoryItemView from "../views/pages/Inventory/InventoryItemView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomePage },
-    { path: '/test', name: 'test_page', component: () => import('../views/pages/TestPage.vue') },
-    { path: '/test-parameter/:id', name: 'test_parameter', component: () => import('../views/pages/TestParameterPage.vue') },
-    { path: '/login', name: 'login', component: () => import('../views/pages/LoginView.vue') },
+    { path: '/login', name: 'login', component: LoginView },
     {
       path: '/products',
       name: 'products_list',
-      component: () => import('../views/pages/Products/ProductList.vue'),
+      component: ProductList,
       beforeEnter: (to, from) => {
         if (!useUserStore().is_logged_in && !AuthProvider.checkCachedToken()) {
           return false;
@@ -23,7 +26,7 @@ const router = createRouter({
     {
       path: '/products/:id',
       name: 'products_view',
-      component: () => import('../views/pages/Products/ProductView.vue'),
+      component: ProductView,
       beforeEnter: (to, from) => {
         if (!useUserStore().is_logged_in && !AuthProvider.checkCachedToken()) {
           return false;
@@ -33,7 +36,7 @@ const router = createRouter({
     {
       path: '/inventory',
       name: 'inventory_list',
-      component: () => import('../views/pages/Inventory/InventoryList.vue'),
+      component: InventoryList,
       beforeEnter: (to, from) => {
         if (!useUserStore().is_logged_in && !AuthProvider.checkCachedToken()) {
           return false;
@@ -43,7 +46,7 @@ const router = createRouter({
     {
       path: '/inventory/:id',
       name: 'inventory_item_view',
-      component: () => import('../views/pages/Inventory/InventoryItemView.vue'),
+      component: InventoryItemView,
       beforeEnter: (to, from) => {
         if (!useUserStore().is_logged_in && !AuthProvider.checkCachedToken()) {
           return false;
